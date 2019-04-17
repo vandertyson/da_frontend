86b6379e7de1fc6157acfc4b06f99eecc1664b08
sonpt <ptson117@gmail.com>
2019-04-17 21:02:29 +0700

    'luist'

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const SERVER = "http://localhost:8080"
 export const URL = {
-  "getQuot":SERVER + "/addQuotations",
+  "getQuot":SERVER + "/quotation/quotations",
   "addQuot": SERVER + "/quotation/addQuotation",
   "getItem": SERVER + "/item/items",
 }
